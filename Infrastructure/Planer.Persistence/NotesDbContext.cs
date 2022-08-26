cb99af8562e36977cdee750b39b607f265712d60
ivenbews <as_lod@mail.ru>
2022-08-26 14:34:25 +0500

    add dbcontext to persistence layer

---
+++ Infrastructure/Planer.Persistence/NotesDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Planer.Application.Interfaces;
+using Planer.Domain;
+using Planer.Persistence.EntityTypeConfiguration;
+
+namespace Planer.Persistence
+{
+    public class NotesDbContext : DbContext, INotesDbContext
+    {
+        public DbSet<Note> Notes { get; set; }
+
+        public NotesDbContext(DbContextOptions<NotesDbContext> options)
+            : base(options) { }
+
+        public Task<int> SaveChangesAsync(CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new NoteConfiguration());
+            base.OnModelCreating(builder);
+        }
+    }
+}
