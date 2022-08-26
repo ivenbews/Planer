7a355336ea172f59ded637f97108dd252b204118
ivenbews <as_lod@mail.ru>
2022-08-26 14:18:34 +0500

    dbcontext interface was added

---
+++ Core/Planer.Application/Interfaces/INotesDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Planer.Domain;
+
+
+namespace Planer.Application.Interfaces
+{
+    public interface INotesDbContext
+    {
+        DbSet<Note> Notes { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken token);
+    }
+}
