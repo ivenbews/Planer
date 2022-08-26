e49e03d055fd040ac1be05d0b605aeae32b3b6d1
ivenbews <as_lod@mail.ru>
2022-08-26 13:56:41 +0500

    Добавьте файлы проекта.

---
+++ Core/Planer.Domain/Note.cs
@@ -0,0 +1,12 @@
+﻿namespace Planer.Domain
+{
+    public class Note
+    {
+        public Guid UserId { get; set; }
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Updated { get; set; }
+    }
+}
